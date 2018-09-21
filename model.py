e8119d7edca5ca732f8a3ff2035e124a2f1ff61a
flrngel <flrngel@gmail.com>
2018-09-22 03:34:22 +0900

    wip

--- model.py
+++ model.py
@@ -1,28 +1,25 @@
 import tensorflow as tf
 
 class CPC():
-    def __init__(self, n=7, batch_size=32, cell_dimension=128, d=7*1024):
+    def __init__(self, n=7, col=7, code_size=1024, cell_dimension=128):
         """
         Autoregressive part from CPC
         ---
-        Placeholder
+        tf.placeholder
             X: input (batch_size, fixed_length, vector dimension)
 
         Argument
             n: padded sequence length
-            batch_size: size of batch
-            cell_dimension: dimension of cell
-            d_row: length of g_encode 
-            d_col: dimension of g_encode
         """
         self.n = n
-        self.batch_size = batch_size
-        self.d = d
-        self.X = tf.placeholder(tf.float32, shape=[None, self.n, self.d])
-        self.mask = tf.placeholder(tf.bool, shape=[(self.n - 1) * self.batch_size])
+        self.col = col
+        self.code_size = code_size
+        self.cell_dimension = cell_dimension
 
-        full_sequence = tf.constant(list(range(1, self.n)) * self.batch_size)
-        sequence_len = tf.boolean_mask(full_sequence, self.mask)
+        self.X = tf.placeholder(tf.float32, shape=[None, self.n * self.col, self.code_size], name='X')
+        assert self.X.shape[1:] == (self.n * self.col, self.code_size)
+        self.X_len = tf.placeholder(tf.float32, shape=[None], name='X_len')
+        self.batch_size = tf.placeholder(tf.int32, shape=[], name='batch_size')
 
         with tf.variable_scope('CPC'):
             cell = tf.contrib.rnn.GRUCell(cell_dimension)
@@ -30,8 +27,8 @@ class CPC():
 
             # Autoregressive model
             with tf.variable_scope('g_ar'):
-                self.ar = tf.nn.dynamic_rnn(cell, self.X, sequence_length=sequence_len, initial_state=initial_state)
-                print(self.ar)
+                _, c_t = tf.nn.dynamic_rnn(cell, self.X, sequence_length=self.X_len, initial_state=initial_state)
+                print(c_t)
 
             # Loss function
             #with tf.variable_scope('train'):
