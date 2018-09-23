dd5b69f1ff00e181564d2c5a915beecca73b55b8
flrngel <flrngel@gmail.com>
2018-09-23 23:53:56 +0900

    frame ready

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+from pprint import pprint
 
 class CPC():
-    def __init__(self, n=7, col=7, code_size=1024, cell_dimension=128):
+    def __init__(self, X, X_len, Y, n=7, code=7, k=2, code_dim=1024, cell_dimension=128):
         """
         Autoregressive part from CPC
         ---
@@ -11,27 +12,42 @@ class CPC():
         Argument
             n: padded sequence length
         """
-        self.n = n
-        self.col = col
-        self.code_size = code_size
-        self.cell_dimension = cell_dimension
-
-        self.X = tf.placeholder(tf.float32, shape=[None, self.n * self.col, self.code_size], name='X')
-        assert self.X.shape[1:] == (self.n * self.col, self.code_size)
-        self.X_len = tf.placeholder(tf.float32, shape=[None], name='X_len')
-        self.batch_size = tf.placeholder(tf.int32, shape=[], name='batch_size')
 
         with tf.variable_scope('CPC'):
-            cell = tf.contrib.rnn.GRUCell(cell_dimension)
+            self.X = X
+            self.X_len = X_len
+            self.Y = Y
+            self.batch_size = X.shape[0]
+            #self.batch_size = tf.placeholder(tf.int32, shape=[], name='batch_size')
+
+            pprint({'X': X, 'X_len': X_len, 'Y': Y, 'batch_size': self.batch_size})
+
+            self.n = n
+            self.k = k
+            self.code = code
+            self.code_dim = code_dim
+            self.cell_dimension = cell_dimension
+
+            #self.X = tf.placeholder(tf.float32, shape=[None, self.n * self.code, self.code_dim], name='X')
+            #self.Y = tf.placeholder(tf.float32, shape=[None, self.k * self.code, self.code_dim], name='Y')
+            #self.X_len = tf.placeholder(tf.float32, shape=[None], name='X_len')
+
+            cell = tf.contrib.rnn.GRUCell(cell_dimension, name='cell')
             initial_state = cell.zero_state(self.batch_size, dtype=tf.float32)
 
             # Autoregressive model
             with tf.variable_scope('g_ar'):
                 _, c_t = tf.nn.dynamic_rnn(cell, self.X, sequence_length=self.X_len, initial_state=initial_state)
-                print(c_t)
+
+            with tf.variable_scope('coding'):
+                predict = []
+                y = tf.reshape(self.Y, [self.batch_size, self.k * self.code * self.code_dim])
+                for i in range(k):
+                    cpc = tf.layers.dense(c_t, k * code * code_dim, name='x_t_'+str(i+1))
+                    out = tf.sigmoid(y * cpc)
+                    predict.append(out)
+                predict = tf.transpose(predict, perm=[1, 0, 2])
 
             # Loss function
-            #with tf.variable_scope('train'):
-            #    self.loss = 
-
-CPC()
+            with tf.variable_scope('train'):
+                self.loss = tf.reduce_mean(tf.subtract(1.0, predict), name='loss', axis=[2,1,0])
--- train.py
+++ train.py
@@ -1,6 +1,49 @@
-from CPC import CPC
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from model import CPC
+from nets.resnet_v2 import resnet_v2_101 as resnet
 
-cpc = CPC()
+learn_rate = 2e-4
+batch_size = 32
 
+def image_preprocess(x):
+    x = tf.expand_dims(x, axis=-1)
+    x = tf.concat([x, x, x], axis=-1)
+    x = tf.image.resize_images(x, (224, 224))
+    return x
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+# load data
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+batches = tf.data.Dataset.from_tensor_slices(train_images).batch(batch_size)
+iterator = batches.make_one_shot_iterator()
+items = iterator.get_next()
+data = image_preprocess(items)
+
+# build graph
+## resnet
+_, features = resnet(data)
+features = features['resnet_v2_101/block3']
+Y = features[:, -2:, : , :]
+X = tf.reshape(features, shape=[batch_size, 7 * 7, 1024])
+
+## cpc
+X_len = [5] * batch_size
+X_len = tf.constant(X_len, dtype=tf.int32)
+
+cpc = CPC(X, X_len, Y)
+train_op = tf.train.AdamOptimizer(learn_rate).minimize(cpc.loss)
+
+# tensorflow
 with tf.Session() as sess:
-    sess.run(cpc.rnn, 
+    sess.run(tf.global_variables_initializer())
+    #sess.run(di.initializer)
+
+    print(sess.run(data).shape)
+
+    _, loss = sess.run([train_op, cpc.loss])
